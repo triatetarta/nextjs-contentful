7ab52eb5815c1a277dae664051cf2ec7010ae268
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-11-30 20:46:41 +0000

    almost done

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
-export default function Recipes() {
+import { createClient } from 'contentful';
+import RecipeCard from '../components/RecipeCard';
+
+export default function Recipes({ recipes }) {
   return (
-    <div className="recipe-list">
-      Recipe List
+    <div className='recipe-list'>
+      {recipes.map((recipe) => {
+        return <RecipeCard key={recipe.sys.id} {...recipe} />;
+      })}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          gap: 20px 60px;
+        }
+      `}</style>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({
+    content_type: 'recipe',
+  });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
 }
