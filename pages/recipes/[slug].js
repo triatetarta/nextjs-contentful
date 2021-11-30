2c280d5e53fb12c4d02f0833a093570b343751ec
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-11-30 21:08:54 +0000

    fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful';
 import Image from 'next/image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Skeleton from '../../components/Skeleton';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -8,6 +9,8 @@ const client = createClient({
 });
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) return <Skeleton />;
+
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
 
@@ -78,7 +81,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
